267579916a73c4ea778131aec63c6c6e0c6db824
Elias Stogiannidis <52886748+stoyian@users.noreply.github.com>
2020-04-27 15:35:23 +0300

    Update PDFMerger.cpp

--- PDFMerger.cpp
+++ PDFMerger.cpp
@@ -1,4 +1,4 @@
-// PDFMaker.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// PDFMerger.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include "podofo.h"
 #include <iostream>
